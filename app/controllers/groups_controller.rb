e6a5dff0210eb30817ffd90e9d34987fc5d4fcf5
melksnitis <mikus.elksnitis@gmail.com>
2021-10-06 17:41:32 +0300

    structure for teams

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,10 +2,7 @@ class GroupsController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
 
   def index
-    file = File.read(File.join(Rails.root, 'app', 'json', 'groups.json'))
-    json = JSON.parse(file)
-
-    render json: json
+    render json: StaticTeams.new.teams
   end
 
   def show
--- lib/static_teams.rb
+++ lib/static_teams.rb
@@ -2,51 +2,75 @@
 
   def teams
     {
-      team2: [
-  			"Aleksej Sokolov",
-  			"Mihkel Alavere",
-  			"Pablo Borges"
-      ],
-      team3: [
-  			"Andrej",
-  			"Guilherme Augusto Alves",
-  			"Ugnė Jaškulytė",
-  			"Žygimantas Jurgelevičius"
-      ],
-      team4: [
-  			"Edvardas Šmulaitis",
-  			"Igor Vaz",
-  			"Lukasz Hajduk"
-      ],
-      team5: [
-  			"Filipe Abreu",
-  			"Manuel Ibáñez Martín",
-  			"Olegs Boldirevs",
-  			"Vyacheslav Belov"
-      ],
-      team6: [
-  			"Eduardo Rebouças",
-  			"Mikus Elksnitis",
-  			"Pedro Rodrigues",
-  			"Tomas Lukas Komar"
-      ],
-      team7: [
-  			"Adolfas Mackonis",
-  			"Arnas Fomenko",
-  			"Marius Jaglauskas",
-  			"Renan Filipe"
-      ],
-      team8: [
-  			"Łukasz Tomczewski",
-  			"Mantas Gineitis",
-  			"Tutku Turkmen",
-  			"Vilius Juodžiukynas"
-      ],		
-      team9: [
-  			"Gustavs Berzins",
-  			"Julius Gerulaitis",
-  			"Mindaugas Jankauskas"
-      ]
+      "1": {
+        name: 'team2',
+        members: [
+          "Aleksej Sokolov",
+          "Mihkel Alavere",
+          "Pablo Borges"
+        ]
+      },
+      "2": {
+        name: 'team3',
+        members: [
+          "Andrej",
+          "Guilherme Augusto Alves",
+          "Ugnė Jaškulytė",
+          "Žygimantas Jurgelevičius"
+        ]
+      },
+      "3": {
+        name: 'team4',
+        members: [
+          "Edvardas Šmulaitis",
+          "Igor Vaz",
+          "Lukasz Hajduk"
+        ]
+      },
+      "4": {
+        name: 'team5',
+        members: [
+          "Filipe Abreu",
+          "Manuel Ibáñez Martín",
+          "Olegs Boldirevs",
+          "Vyacheslav Belov"
+        ]
+      },
+      "5": {
+        name: 'team6',
+        members: [
+          "Eduardo Rebouças",
+          "Mikus Elksnitis",
+          "Pedro Rodrigues",
+          "Tomas Lukas Komar"
+        ]
+      },
+      "6": {
+        name: 'team7',
+        members: [
+          "Adolfas Mackonis",
+          "Arnas Fomenko",
+          "Marius Jaglauskas",
+          "Renan Filipe"
+        ]
+      },
+      "7": {
+        name: 'team8',
+        members: [
+          "Łukasz Tomczewski",
+          "Mantas Gineitis",
+          "Tutku Turkmen",
+          "Vilius Juodžiukynas"
+        ]
+      },
+      "8": {
+        name: 'team9',
+        members: [
+          "Gustavs Berzins",
+          "Julius Gerulaitis",
+          "Mindaugas Jankauskas"
+        ]
+      }
     }
   end
  end
