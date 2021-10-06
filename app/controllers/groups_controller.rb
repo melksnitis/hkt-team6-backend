f19d5c1cfb7614a88bcd2249487366360c12b196
melksnitis <mikus.elksnitis@gmail.com>
2021-10-06 18:44:53 +0300

    change structure

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,22 +2,19 @@ class GroupsController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
 
   def index
-    render json: StaticTeams.new.teams
-  end
+    teams = StaticTeams.new.teams
+    teams.each do |team|
+      new_team_members = []
+      client = Slack::Web::Client.new
+      members = client.users_list.members
+      new_team_members = []
+      members.select {|a| team[:members].include?(a.real_name)}.each do |member|
+        new_team_members << {name: member.real_name, image_url: member.profile.image_512}
+      end
+      team[:members] = new_team_members
+    end 
 
-  def show
-    team = StaticTeams.new.teams.select {|a| a[:id] == params["id"].to_i}.first
-    return unless team
-
-    client = Slack::Web::Client.new
-    members = client.users_list.members
-    new_team_members = []
-    members.select {|a| team[:members].include?(a.real_name)}.each do |member|
-      new_team_members << {name: member.real_name, image_url: member.profile.image_512}
-    end
-    team[:members] = new_team_members
-
-    render json: team
+    render json: teams
   end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   jsonapi_resources :users
-  # resources :groups, only: [:inex]
-  get "groups/:id",  to: 'groups#show', format: :json
+
   get "groups",  to: 'groups#index'
 end
