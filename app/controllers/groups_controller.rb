87274e576779167cf6ddf876d699830f766ae42a
melksnitis <mikus.elksnitis@gmail.com>
2021-10-06 21:09:31 +0300

    base 32

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
+  require 'open-uri'
 
   def index
     teams = StaticTeams.new.teams
@@ -8,8 +9,10 @@ class GroupsController < JSONAPI::ResourceController
       client = Slack::Web::Client.new
       members = client.users_list.members
       new_team_members = []
+
       members.select {|a| team[:members].include?(a.real_name)}.each do |member|
-        new_team_members << {name: member.real_name, image_url: member.profile.image_512}
+        puts member.real_name
+        new_team_members << {name: member.real_name, image_url: "data:image/png;base64,#{Base64.encode64(URI.open(member.profile.image_32).read).gsub("\n", '')}"}
       end
       team[:members] = new_team_members
     end 
